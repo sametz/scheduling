c914b97ea469e1f77a546d4c62c9fd3a3d89ec45
Geoffrey M. Sametz <sametz@udel.edu>
2017-12-22 23:07:13 -0600

    update_classes_gms.py is now a standalone script for saving Spring 2018 class data as a csv, with test_update_classes_gms.py complete as well.

--- interface/test_update_classes_gms.py
+++ interface/test_update_classes_gms.py
@@ -1,3 +1,6 @@
+import csv
+import os
+import re
 import unittest
 
 import requests
@@ -7,7 +10,7 @@ import update_classes_gms as ucg
 
 class UpdateClasses(unittest.TestCase):
     def setUp(self):
-        requests_cache.install_cache('scheduling', \
+        requests_cache.install_cache('scheduling',
                                      backend='sqlite', expire_after=7200)
         self.first_page, self.status = ucg.get_class_page(ucg.CLASS_URL)
         self.assertEqual(200, self.status)
@@ -36,3 +39,68 @@ class UpdateClasses(unittest.TestCase):
                           'Location',
                           'Instructor'])
 
+    def test_get_table_data(self):
+        table = ucg.find_table(self.first_page)
+        table_data = ucg.get_table_data(table)
+        first_cell = table_data[0][0]
+        self.assertTrue(first_cell.startswith('CHEM'))
+
+    def test_get_next_url(self):
+        current_url = ucg.CLASS_URL
+        next_url = ucg.get_next_url(self.first_page, current_url)
+        next_page, status = ucg.get_class_page(next_url)
+        self.assertEqual(200, status)
+
+    def test_process_pages(self):
+        current_url = ucg.CLASS_URL
+        current_page, status = ucg.get_class_page(current_url)
+        current_table = ucg.find_table(current_page)
+        data = []
+        ucg.process_pages(current_page, current_table, current_url, data)
+        first_enrollment = data[0][0][2]
+        enrollment = re.compile('\d+ OF \d+')
+        result = enrollment.search(first_enrollment)
+        print result.group()
+        self.assertRegexpMatches(first_enrollment, enrollment)
+
+    def test_scrape_pages(self):
+        current_url = ucg.CLASS_URL
+        headers, data = ucg.scrape_pages(current_url)
+        self.assertEqual(headers,
+                         ['Course',
+                          'Title',
+                          'Open seats',
+                          'Credits',
+                          'Day',
+                          'Time',
+                          'Location',
+                          'Instructor'])
+        first_enrollment = data[0][0][2]
+        enrollment = re.compile('\d+ OF \d+')
+        result = enrollment.search(first_enrollment)
+        print result.group()
+        self.assertRegexpMatches(first_enrollment, enrollment)
+
+    def test_convert_page_data(self):
+        current_url = ucg.CLASS_URL
+        headers, data = ucg.scrape_pages(current_url)
+        converted_data = ucg.convert_page_data(data)
+        first_enrollment = converted_data[0][2]
+        enrollment = re.compile('\d+ OF \d+')
+        result = enrollment.search(first_enrollment)
+        print result.group()
+        self.assertRegexpMatches(first_enrollment, enrollment)
+
+    def test_save_csv(self):
+        headers = ['a', 'b']
+        data = [[3, 7], [11, 13]]
+        ucg.save_csv(headers, data, 'test.csv')
+        results = open('test.csv')
+        results_reader = csv.reader(results)
+        results_data = list(results_reader)
+        print results_data
+        self.assertEqual(results_data,
+                         [['a', 'b'],
+                          ['3', '7'],
+                          ['11', '13']])
+        os.remove('test.csv')
--- interface/update_classes_gms.py
+++ interface/update_classes_gms.py
@@ -4,23 +4,23 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-CLASS_URL = ("https://udapps.nss.udel.edu/CoursesSearch/search-results" 
-"?term=2183"
-"&search_type=A"
-"&course_sec=CHEM"
-"&session=All"
-"&course_title="
-"&instr_name="
-"&text_info=All"
-"&instrtn_mode=All"
-"&time_start_hh="
-"&time_start_ampm="
-"&credit=Any"
-"&keyword="
-"&subj_area_code="
-"&college="
-"&startat="
-)
+CLASS_URL = ("https://udapps.nss.udel.edu/CoursesSearch/search-results"
+             "?term=2183"
+             "&search_type=A"
+             "&course_sec=CHEM"
+             "&session=All"
+             "&course_title="
+             "&instr_name="
+             "&text_info=All"
+             "&instrtn_mode=All"
+             "&time_start_hh="
+             "&time_start_ampm="
+             "&credit=Any"
+             "&keyword="
+             "&subj_area_code="
+             "&college="
+             "&startat="
+             )
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
 
@@ -32,9 +32,78 @@ def get_class_page(url):
 def find_table(page):
     return page.find_all('table')[0]
 
+
 def find_table_headers(table):
     return [th.text.encode('ascii') for th in table.select('th')]
 
 
+def get_table_data(table):
+    """Parse a bs4 table selection row by row to return a list of lists.
+        :param: table: bs4 table element
+        :returns: a list of lists of td elements, analogous to a list of spreadsheet rows"""
+    table_rows = table.select('tr')
+    table_data = [[td.text.strip().encode('ascii') for td in row.select('td')]
+                  for row in table_rows[1:]]
+    return table_data
+
+
+def get_next_url(page, current_url):
+    next_button = page.find(id='searchNxtBtn')
+    if next_button is None:
+        print 'end reached'
+        return None
+    print 'current url: ', current_url
+    next_url_hint = next_button['onclick']
+    startat = re.compile('&startat=[^&\']*')
+    print 'searching: ', next_url_hint
+    new_startat = startat.search(next_url_hint).group()
+    print 'new startat: ', new_startat
+
+    new_url = startat.sub(new_startat, current_url)
+    print 'new url: ', new_url
+    return new_url
+
+
+def process_pages(current_page, current_table, current_url, data):
+    data.append(get_table_data(current_table))
+    new_url = get_next_url(current_page, current_url)
+    if new_url == None:
+        return
+    else:
+        print 'old url: ', current_url[-25:]
+        print 'new_url: ', new_url[-25:]
+        new_page, status = get_class_page(new_url)
+        new_table = find_table(new_page)
+        process_pages(new_page, new_table, new_url, data)
+
+
+def scrape_pages(current_url):
+    current_page, status = get_class_page(current_url)
+    all_data = []
+    #     at_end = False
+    current_table = find_table(current_page)
+    headers = find_table_headers(current_table)
+    print 'headers: ', headers
+    process_pages(current_page, current_table, current_url, all_data)
+
+    return headers, all_data
+
+
+def convert_page_data(data):
+    result = []
+    for page in data:
+        for row in page:
+            result.append(row)
+    return result
+
+
+def save_csv(headers, data, filename):
+    df = pd.DataFrame.from_records(data, columns=headers)
+    df.to_csv(filename, index=False)
+
+
 if __name__ == '__main__':
     current_url = CLASS_URL
+    headers, data = scrape_pages(current_url)
+    good_data = convert_page_data(data)
+    save_csv(headers, good_data, 'test_spring18.csv')
